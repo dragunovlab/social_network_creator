4f74c4e6959370eacb3596b6b864c6a111a6edbe
dragunov.lab <dragunov.lab@gmail.com>
2022-02-21 17:32:16 +0300

    06. Уроки React JS (index.js, App, JSX)

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+const App = () => {
+  return (
+  <div>
+  <div>one div</div>
+  <div className='App'>
+    <ul>
+      <li>html</li>
+      <li>css</li>
+      <li>js</li>
+      <li>react</li>
+    </ul>
+  </div>
+  </div> 
+  );  
+ }
+
+export default App; 
